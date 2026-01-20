19da284ed6a06cd962933da05c21cc1b887c508f
Mrkm6062 <akm222143@gmail.com>
2026-01-20 22:11:26 +0530

    resolved final

--- routes/Contact.js
+++ routes/Contact.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Contact = require('../models/Contact');
 
-// POST /api/contact
+// GET /api/contact
 router.get('/', async (req, res) => {
   try {
     const contacts = await Contact.find();
